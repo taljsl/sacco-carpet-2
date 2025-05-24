711c6283b7c798ff3b4aecc69ac84c9aa0c39981
Josh L <joshua.levine42@gmail.com>
2025-05-24 17:41:59 -0400

    Added Box Shadows to Nav Bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,21 @@ export default function Header() {
     <header className="p-2 flex gap-2 bg-white text-black justify-between items-center">
       <nav className="flex flex-row space-x-4 px-2 items-center">
         <Link to="/">
-          <img src={saccologo} alt="Sacco Carpet Logo" className="h-[80px] w-auto" />
+          <img
+            src={saccologo}
+            alt="Sacco Carpet Logo"
+            className="h-[80px] w-auto"
+          />
         </Link>
-        <Link to="/shop" className="hover:bg-gray-100">SHOP</Link>
-        <Link to="/installations">INSTALLATIONS</Link>
-        <Link to="/about">ABOUT US</Link>
+        <Link to="/shop" className="hover:bg-gray-100">
+          SHOP
+        </Link>
+        <Link to="/installations" className="hover:bg-gray-100">
+          INSTALLATIONS
+        </Link>
+        <Link to="/about" className="hover:bg-gray-100">
+          ABOUT US
+        </Link>
       </nav>
     </header>
   )
