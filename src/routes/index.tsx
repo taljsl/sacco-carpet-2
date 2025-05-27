24f5f62af5323386377be2f16d2f5ec6c3609885
Josh L <joshua.levine42@gmail.com>
2025-05-27 15:48:54 -0400

    Have now removed both videos that presumably prevented github uploads, attempting new push

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import saccoHomePageVideo from '../assets/saccoHomePageVideo.mp4'
+// import saccoHomePageVideo from '../assets/saccoHomePageVideo.mp4'
 import ResidentialCard from '@/components/ResidentialCard'
 import HospitalityCard from '@/components/HospitalityCard'
 
@@ -12,7 +12,7 @@ function App() {
     <>
       <div className="w-screen h-auto border-[3px] border-blue-500">
         <video
-          src={saccoHomePageVideo}
+          src="https://prod-sacco.s3.amazonaws.com/video/sacco-video-header-v2.mp4?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARBILXDGNIVPQ2CP3%2F20250527%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250527T194739Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=f55ae2dc9ddd036b6024b6b9404eb40b5167eeddbc07830b7cf40baa73ab79b8"
           className="w-full h-auto"
           autoPlay
           loop
