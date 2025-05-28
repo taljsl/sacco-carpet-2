3a7af96b34f9536e59b98c4e859df85fc7d00cce
Josh L <joshua.levine42@gmail.com>
2025-05-28 14:18:11 -0400

    First install page done, not styled

--- src/routes/view-installation/1.tsx
+++ src/routes/view-installation/1.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Num1Home from '../../assets/individualPages/Num1Home.jpg'
+import Num1Sub1 from '../../assets/individualPages/Num1Sub1.jpg'
 
 export const Route = createFileRoute('/view-installation/1')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/view-installation/1"!</div>
+  return (
+    <div className="container">
+      <div>
+        <img src={Num1Home} alt="" />
+      </div>
+      <div>{/* divider goes here */}</div>
+      <div>
+        <h3>BREAKERS PALM BEACH</h3>
+      </div>
+      <div>
+        <img src={Num1Sub1} alt="" />
+        <span>ID:76620</span>
+      </div>
+    </div>
+  )
 }
