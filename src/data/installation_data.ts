96e213c3b1e7975c06834fb3ccce69e9dedc2341
Josh L <joshua.levine42@gmail.com>
2025-05-29 00:43:05 -0400

    Grabbed all images and id's from installations and input into data

--- src/data/installation_data.ts
+++ src/data/installation_data.ts
@@ -24,49 +24,59 @@ export const pictures: Array<pictureData> = [
   },
   {
     id: 3,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num3Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num3Sub1.jpg', picid: 'ID: 92343' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'EDITIONS WEST HOLYWOOD',
   },
+
   {
     id: 4,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num4Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num4Sub1.jpg', picid: 'ID: 24601' },
+      { src: '../src/assets/individualpages/Num4Sub2.jpg', picid: 'ID: 30249' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'FOUR SEASONS SAN FRANCISCO',
   },
   {
     id: 5,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num5Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num5Sub1.jpg', picid: 'ID: 96137' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'KABUKI HOTEL',
   },
+
   {
     id: 6,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num6Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      {
+        src: '../src/assets/individualpages/Num6Sub1.jpg',
+        picid: 'ID: 106832',
+      },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'GRAND HYATT NASHVILLE',
   },
   {
     id: 7,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num7Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      {
+        src: '../src/assets/individualpages/Num7Sub1.jpg',
+        picid: 'ID: 104477',
+      },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'ONE HOTEL TORONTO',
   },
+
   {
     id: 8,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num8Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num8Sub1.jpg', picid: 'ID: 88669' },
     ],
     title: 'BREAKERS PALM BEACH',
   },
@@ -82,47 +92,60 @@ export const pictures: Array<pictureData> = [
         src: '../src/assets/individualpages/Num9Sub2.jpg',
         picid: 'ID: 108267',
       },
+      {
+        src: '../src/assets/individualpages/Num9Sub3.jpg',
+        picid: 'ID: 108268',
+      },
+      {
+        src: '../src/assets/individualpages/Num9Sub4.jpg',
+        picid: 'ID: 108269',
+      },{
+        src: '../src/assets/individualpages/Num9Sub5.jpg',
+        picid: 'ID: 108270',
+      },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'ENLIGHTENED TIGER COLLECTION',
   },
   {
     id: 10,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num10Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num10Sub1.jpg', picid: 'ID: 107719' },
+      { src: '../src/assets/individualpages/Num10Sub2.jpg', picid: 'ID: 95047' },
+      { src: '../src/assets/individualpages/Num10Sub3.jpg', picid: 'ID: 22603' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'FOX NAHEM DESIGN',
   },
   {
     id: 11,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num11Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num11Sub1.jpg', picid: 'ID: 89722' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'PARIS FORINO YELLOWWOOD',
   },
   {
     id: 12,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num12Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num12Sub1.jpg', picid: 'ID: 108602' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'LANIA LANE MASTER',
   },
   {
     id: 13,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num13Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num13Sub1.jpg', picid: 'ID: 21705' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'LANIA LANE LIVING',
   },
   {
     id: 14,
-    homeImg: '../src/assets/individualpages/Num1Home.jpg',
+    homeImg: '../src/assets/individualpages/Num14Home.jpg',
     images: [
-      { src: '../src/assets/individualpages/Num1Sub1.jpg', picid: 'ID:76620' },
+      { src: '../src/assets/individualpages/Num14Sub1.jpg', picid: 'ID: 196939' },
     ],
-    title: 'BREAKERS PALM BEACH',
+    title: 'GREIG ENTRY RUG LIVING ROOM RUG',
   },
 ]
